6dd219264ff66a6e4dc6608299d29a125b9c35cd
Chen <kdd397@dca90475f397.cof.ds.capitalone.com>
2017-07-20 10:33:47 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,22 +2,24 @@ console.log("Up and running!");
 
 
 var cards = ['queen', 'queen', 'king', 'king'];
-
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log(cardOne + " has been flipped.");
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log(cardTwo + " has been flipped.");
-
-if(cardsInPlay.length === 2) {
-  if(cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  }
-  else {
-    alert("Sorry, try again");
+checkForMatch = function(){
+  if(cardsInPlay.length === 2) {
+    if(cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    }
+    else {
+      alert("Sorry, try again");
+    }
   }
 }
+
+flipCard = function(cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
